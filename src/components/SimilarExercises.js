82a5b2ca6d496138b41c8fee777161ae10c790d8
Lucia Moral <lmoralbaena@gmail.com>
2023-10-03 16:12:09 +0200

    amended typo horizontalscrollbar

--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Box, Stack, Typography } from "@mui/material";
-import HorizontalScrollbar from "./HorizontalScrollbar";
+import HorizontalScrollbar from "./HorizontalScrollba";
 
 const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => (
   <Box sx={{ mt: { lg: "100px", xs: "0px" } }}>
